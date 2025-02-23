7a5d55d880d67cc5efa84498597b38f05e6e009b
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-02-23 10:28:08 +0000

    Update examples workings

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,15 @@
-len("Hello")
+hello_string = "Hello"
+print(hello_string)
+print(len(hello_string))
+last_char = hello_string[-1]
+print(last_char)
+large_number_with_underscode = 1_000_000
+print(large_number_with_underscode)
+large_float_with_underscode = 1_000_000.111_111
+print(large_float_with_underscode)
+bool_with_uppercase = True
+print(bool_with_uppercase)
+# bool_with_lowercase = true # NameError: name 'true' is not defined
+
+
+
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,16 @@
-len(12345)
+
+hello_string = "Hello, World!"
+print(type(hello_string))
+
+hello_bool = True
+print(type(hello_bool))
+
+hello_int = 11
+print(type(hello_int))
+
+hello_float = 11.222
+print(type(hello_float))
+
+input_name = len(input("Enter your name:\n"))
+print(type(input_name))
+print("Number of letters in your name: " + str(input_name))
