e5be2d6f0b73680eb77f334e2570067916f46bad
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-02-23 15:07:22 +0000

    Update examples workings

--- Day 2/Mathematical Operations/task.py
+++ Day 2/Mathematical Operations/task.py
@@ -1,1 +1,19 @@
 print("My age: " + str(12))
+print(123 + 456)
+print(123 - 22)
+print(3 * 2)
+t1 = 6 / 3
+print(t1)
+print(type(t1))
+divide_then_remove_decimal_values = 6 // 3
+print(divide_then_remove_decimal_values)
+print(type(divide_then_remove_decimal_values))
+
+
+power_value = 4**2
+print(power_value)
+print(type(power_value))
+
+print((3 * 3) + (3 / 3) - 3)
+
+print(3 * ((3 + (3 / 3)) - 3))
