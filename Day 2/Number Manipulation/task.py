769fb3ac57531e10f5f6a66de15f9d5baf7e26c7
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2025-03-02 14:31:34 +0000

    Update examples workings

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,9 @@
 bmi = 84 / 1.65 ** 2
+
+print(type(bmi))
+print(bmi)
+
+print(int(bmi))
+print(round(bmi))
+print(round(bmi, 3))
+print(f"{bmi} value")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -3,4 +3,7 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to give? 10 12 15 "))
 people = int(input("How many people to split the bill? "))
 
+total_to_pay = (tip * bill) + bill
+tip_per_person = total_to_pay / people
+print(f"total_to_pay ${total_to_pay} tip_per_person ${tip_per_person}")
 
